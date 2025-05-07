65e3c5f692ce3ebdc77ce5325f9299733b03c4a9
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-05-07 17:47:11 +0900

    fix: textArea 공통 컴포넌트에 maxLength, placeholder props 추가 및 maxLength에 해당하는 textArea 높이 지정

--- src/components/inputs/TextArea.tsx
+++ src/components/inputs/TextArea.tsx
@@ -3,21 +3,24 @@ import React from 'react';
 interface Props {
 	inputData: string;
 	onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
+	maxLength: 200 | 500 | 2000;
+	placeholder: string;
 }
 
-function TextArea({ inputData, onChange }: Props) {
+function TextArea({ inputData, onChange, maxLength, placeholder }: Props) {
+	const height = maxLength === 200 ? 'h-[101px]' : 'h-[192px]'
 	return (
 		<div className='w-full flex flex-col gap-[2px] items-end'>
 			<textarea
-				className='w-full h-[192px] focus:outline-none border border-black p-[14px] text-[14px] font-medium text-[#010101] resize-none'
+				className={`w-full ${height} focus:outline-none border border-black p-[14px] text-[14px] font-medium text-[#010101] resize-none`}
 				value={inputData}
 				onChange={onChange}
-				maxLength={500}
-				placeholder='서비스에 대해서 자세하게 적어주세요.'
+				maxLength={maxLength}
+				placeholder={placeholder}
 			/>
 			<div className='text-[14px] font-medium text-[#010101]'>
 				{inputData.length}
-				<span className='text-[#B3B3B3]'>/500</span>
+				<span className='text-[#B3B3B3]'>/{maxLength}</span>
 			</div>
 		</div>
 	);
