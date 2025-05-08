feda7e0c4e2e18ef6c8560fdd4e5f43f35478fc5
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-05-08 19:43:36 +0900

    refactor: /user경로의 store관련 폴더 및 파일명 -> business로 수정

---
+++ src/app/user/view-business/[businessId]/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+
+function UserViewBusiness() {
+	return <div>UserViewBusiness</div>;
+}
+
+export default UserViewBusiness;
--- src/app/user/view-store/[storeId]/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import React from 'react';
-
-function UserViewStore() {
-	return <div>UserViewStore</div>;
-}
-
-export default UserViewStore;
