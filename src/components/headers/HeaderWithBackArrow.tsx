8771d7cbbc8dab85cefc04e8b8fd1014288df3ea
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-05-09 20:07:45 +0900

    fix: 뒤로가기버튼있는 헤더에서 확인버튼 뿐만 아니라 정보수정 버튼도 있어야하기 때문에 type props를 추가하여 Type에 맞게 보여지도록 수정

--- src/components/headers/HeaderWithBackArrow.tsx
+++ src/components/headers/HeaderWithBackArrow.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import CompleteButton from '../buttons/CompleteButton';
+import { MiniButton } from '../buttons/MiniButton';
 
 interface Props {
 	headerTitle: string;
 	handleBackArrowClick: () => void;
 	hasRightConfirmButton: boolean;
-	handleConfirmButtonClick?: () => void;
+	handleRightButtonClick?: () => void;
+	type?: "확인" | "수정"
 }
 
 function HeaderWithBackArrow({
 	headerTitle,
 	handleBackArrowClick,
 	hasRightConfirmButton,
-	handleConfirmButtonClick,
+	handleRightButtonClick,
+	type = "확인"
 }: Props) {
 	return (
 		<div className='w-full h-[51px] flex items-center relative text-black bg-white'>
@@ -26,10 +29,14 @@ function HeaderWithBackArrow({
 			<div className='text-[20px] font-black ml-[15px]'>{headerTitle}</div>
 			{hasRightConfirmButton && (
 				<div className='ml-auto'>
-					<CompleteButton
+					{type === "확인" ? <CompleteButton
 						size='small'
-						handleClick={handleConfirmButtonClick!}
+						handleClick={handleRightButtonClick!}
 					/>
+						:
+						<MiniButton buttonText='정보 수정' handleClick={handleRightButtonClick!} isClicked={true} />
+				}
+					
 				</div>
 			)}
 		</div>
