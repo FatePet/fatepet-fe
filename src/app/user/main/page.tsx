f71b696ba9ff5a879e173da4d5f9c3d3ae0d9978
naga361111 <naga361111@gmail.com>
2025-05-12 20:30:57 +0900

    메인 페이지 navbar

--- src/app/user/main/page.tsx
+++ src/app/user/main/page.tsx
@@ -1,8 +1,48 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import LocationBar from '@/components/location/LocationBar';
+import RegisterLocationModal from '@/components/modals/RegisterLocationModal';
+import ModalLayout from '@/components/modals/ModalLayout';
 
 function UserMain() {
-	return <div>UserMain</div>;
+	// 전역 location 값
+	const location = '';
+
+	const [isRegisterLocationModalOpen, setIsRegisterLocationModalOpen] = useState<boolean>(false);
+	const handleRegisterLocationModal = () => {
+		setIsRegisterLocationModalOpen(true);
+	};
+
+	return (
+		<div>
+			<div className='mt-3'>
+				<Image
+					src='/logo/fatepetLogo.svg'
+					alt='fatepet Logo'
+					width={120}
+					height={120}
+				/>
+			</div>
+
+			<div className='min-w-[320px] max-[600px] -mx-[16.2px] my-3'>
+				<LocationBar
+					location={location}
+					handleClick={handleRegisterLocationModal}
+				/>
+			</div>
+
+			{isRegisterLocationModalOpen && (
+				<ModalLayout setIsModalOpen={setIsRegisterLocationModalOpen}>
+					<RegisterLocationModal 
+						setIsModalOpen={setIsRegisterLocationModalOpen}
+						location={location}
+					/>
+				</ModalLayout>
+			)}
+		</div>
+
+	)
 }
 
 export default UserMain;
