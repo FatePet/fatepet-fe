104c05d4dc46aec015772e700363a0b94e573004
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-05-15 23:05:54 +0900

    Style: buttonText 컴포넌트에 "업체 삭제", "취소" 추가

--- src/components/buttons/ModalButton.tsx
+++ src/components/buttons/ModalButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 interface Props {
-	buttonText: '등록 취소' | '계속 입력' | '확인';
+	buttonText: '등록 취소' | '계속 입력' | '확인' | "업체 삭제" | '취소';
 	handleClick: () => void;
 }
 
@@ -9,11 +9,11 @@ function ModalButton({ buttonText, handleClick }: Props) {
 	let className = 'h-[30px] rounded-[4px] font-bold text-[14px] cursor-pointer text-white';
 	if (buttonText === '확인') {
 		className += ' w-[65px] bg-p-black';
-	} else if (buttonText === '등록 취소') {
+	} else if (buttonText === '등록 취소' || buttonText === "업체 삭제") {
 		className += ' w-[92px] bg-p-red';
-	} else if (buttonText === '계속 입력') {
+	} else if (buttonText === '계속 입력' || buttonText === '취소') {
 		className += ' w-[92px] bg-p-black ';
-	}
+	} 
 
 	return (
 		<button className={className} onClick={handleClick}>
