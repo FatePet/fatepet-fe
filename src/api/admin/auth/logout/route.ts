de23d8bb76e1de5ac2b7ff8c5afa3ab1f6edfc6d
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-05-16 20:29:48 +0900

    fix: 로그아웃 api 로직 응답형식 공통 응답 형식으로 수정

--- src/api/admin/auth/logout/route.ts
+++ src/api/admin/auth/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function POST() {
-	const response = NextResponse.json({ success: true });
+	const response = NextResponse.json({ status: 200, message: '로그아웃 성공' });
 
 	// refresh 쿠키 삭제
 	response.cookies.set('refresh', '', {
