15c8fa380247d46aa2417ed1c48df7117c6009c2
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-05-16 18:55:20 +0900

    feat: 관리자 로그인 api 연동 로직 작성

---
+++ src/api/admin/auth/postAdminLogin.ts
@@ -0,0 +1,45 @@
+import { apiRoutes } from '@/_lib/apiRoutes';
+import { useMutation } from '@tanstack/react-query';
+
+export const postAdminLogin = async (
+	body: IPostLoginRequestType,
+): Promise<string> => {
+	const endpoint = `${process.env.NEXT_PUBLIC_API}${apiRoutes.login}`;
+
+	const requestOptions: RequestInit = {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(body),
+		credentials: 'include',
+	};
+
+	const response = await fetch(endpoint, requestOptions);
+
+	if (!response.ok) {
+		const result = await response.json();
+		throw new Error(result.message);
+	}
+	console.log([...response.headers.entries()]);
+	const authorization = response.headers.get(
+		'Authorizationx-amzn-Remapped-Authorization',
+	);
+	if (!authorization) {
+		throw new Error('Authorization header is missing');
+	}
+
+	const accessToken = authorization.split(' ')[1];
+	if (!accessToken) {
+		throw new Error('Access token is missing in the authorization header');
+	}
+
+	return accessToken;
+};
+
+export const usePostAdminLogin = () => {
+	return useMutation({
+		mutationFn: (body: IPostLoginRequestType) => postAdminLogin(body),
+	});
+};
