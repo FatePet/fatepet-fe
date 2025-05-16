beb87526b4d3d015c6a05fb0004a3d99ce9b4f78
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-05-16 18:55:52 +0900

    chore: 인증 관련 전역 변수 저장 로직 수정 및 타입 수정

---
+++ src/store/useAuthStore.tsx
@@ -0,0 +1,30 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface AuthState {
+	isLogin: boolean;
+	accessToken: string;
+	setIsLogin: (isLogin: boolean) => void;
+	setAccessToken: (accessToken: string) => void;
+	clearAuth: () => void;
+}
+
+const useAuthStore = create<AuthState>()(
+	persist(
+		(set) => ({
+			isLogin: false,
+			accessToken: '',
+			setIsLogin: (isLogin) => set({ isLogin }),
+			setAccessToken: (accessToken) => set({ accessToken }),
+			clearAuth: () => {
+				set({ isLogin: false, accessToken: '' });
+				localStorage.removeItem('auth-storage');
+			},
+		}),
+		{
+			name: 'auth-storage',
+		},
+	),
+);
+
+export default useAuthStore;
--- src/types/admin/LoginTypes.d.ts
+++ src/types/admin/LoginTypes.d.ts
@@ -1,8 +1,5 @@
 // 관리자 로그인을 처리하기 위한 POST 메소드 타입
-type IPostLoginResquestType = {
+type IPostLoginRequestType = {
     username: string;
     password: string;
-};
-type IPostLoginResponseType = {
-    success: boolean;
 };
