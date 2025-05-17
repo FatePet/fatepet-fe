8d2cd25af9781d622587612608351e6e335df5ff
shuding0307 <suhyun2116@gmail.com>
2025-05-17 17:01:00 +0900

    style: 프리텐다드 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from 'next';
 import './styles/globals.css';
 import RQProvider from '@/components/RQProvider';
 import { Suspense } from 'react';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+	src: '../../public/fonts/PretendardVariable.woff2',
+	display: 'swap',
+	weight: '45 920',
+	variable: '--font-pretendard',
+});
 
 export const metadata: Metadata = {
 	title: 'FatePet',
