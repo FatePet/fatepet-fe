674f49a773c2b8a93df65f2e1f0e31b32a64699a
naga361111 <naga361111@gmail.com>
2025-05-18 17:43:26 +0900

    style: icon 추가

--- src/app/user/main/_components/UserCategory.tsx
+++ src/app/user/main/_components/UserCategory.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 interface Props {
     upperText: string;
@@ -8,21 +9,26 @@ interface Props {
     category: string;
 }
 
-function UserCategory({ upperText, lowerText, iconUrl, category }: Props) {
+function UserCategory({ upperText, lowerText, category }: Props) {
     const [themeColor, setThemeColor] = useState<string>('');
+    const [iconUrl, setIconUrl] = useState<string>('');
     useEffect(() => {
         switch (category) {
             case '장묘':
                 setThemeColor('bg-[var(--p-brown)]');
+                setIconUrl('/icons/userCategory/icon-rainbow.png')
                 break;
             case '악세사리':
                 setThemeColor('bg-[#DA7F4D]');
+                setIconUrl('/icons/userCategory/icon-acc.png')
                 break;
             case '브리더':
                 setThemeColor('bg-[#F5AB49]');
+                setIconUrl('/icons/userCategory/icon-breeder.png')
                 break;
             case '행동상담':
                 setThemeColor('bg-[#9BB069]')
+                setIconUrl('/icons/userCategory/icon-counsell.png')
                 break;
         }
     }, [])
@@ -38,22 +44,32 @@ function UserCategory({ upperText, lowerText, iconUrl, category }: Props) {
                 router.push(`/main/view-business/${category}/${businessId}`);
                 break;
             case '악세사리':
-                router.push('user/main/ac');
+                router.push('user/main/acc');
                 break;
             case '브리더':
-                router.push('user/main/br');
+                router.push('user/main/bree');
                 break;
             case '행동상담':
-                router.push('user/main/act');
+                router.push('user/main/counsell');
                 break;
         }
     };
 
     return (
-        <div className={`${themeColor} p-7 rounded-2xl font-bold`} onClick={handleCategoryClick}>
+        <div className={`relative ${themeColor} p-7 rounded-2xl font-bold`} onClick={handleCategoryClick}>
             {upperText}<br></br>{lowerText}
-            {/* icon 배치로 수정 */}
-            {iconUrl}
+
+            {iconUrl !== '' && (
+                <Image
+                    src={iconUrl}
+                    alt={`${category} icon`}
+                    width={76}
+                    height={76}
+                    className="absolute bottom-10 right-10"
+                />
+            )}
+
+
         </div>
     );
 };
