1d3acbeaf8de5f00fa4e7716419903892558ec39
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-05-20 19:57:12 +0900

    refactor: 타입 파일 위치 변경

---
+++ src/types/common/Business.d.ts
@@ -0,0 +1,44 @@
+// 업체 상세 조회 페이지, 업체 목록 조회 페이지에서 공통적으로 사용되는 데이터 타입
+type IBusinessItemCommonType = {
+	name: string;
+	address: string;
+	mainImageUrl: string;
+	businessHours: string;
+	phoneNumber: string;
+	category: string;
+};
+
+// 업체 상세 조회
+type IGetBusinessDetailRequestType = {
+	businessId: string;
+	authorization?: string;
+};
+
+type IGetBusinessDetailResponseType = IResponseType & {
+	data: IBusinessDetailDataType;
+};
+
+type IBusinessDetailDataType = IBusinessItemCommonType & {
+	email: string;
+	services: IServiceDetailType[];
+	additionalInfo: IAdditionalInfoType;
+};
+
+type IServiceDetailType = {
+    serviceId: number;
+	category: string;
+	name: string;
+	description: string;
+	imageUrl: string;
+	price: string;
+};
+
+type IAdditionalInfoType = {
+	images: IAdditionalImageType[];
+	description: string;
+};
+
+type IAdditionalImageType = {
+    imageId: number;
+    url: string;
+}
