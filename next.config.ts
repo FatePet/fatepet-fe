44770805b7c669a47c63387dd86448d7bba538df
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-05-22 16:29:34 +0900

    chore: 개발환경일때 proxy 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	async rewrites() {
+		if (process.env.NODE_ENV === 'development') {
+			return [
+				{
+					source: 'api/:path*',
+					destination: `${process.env.NEXT_PUBLIC_API}/:path*`,
+				},
+			];
+		}
+		return [];
+	},
 };
 
 export default nextConfig;
