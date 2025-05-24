e0f2e61e2993c46605ced6f7cece4e9b966c59ae
shuding0307 <suhyun2116@gmail.com>
2025-05-24 23:55:26 +0900

    feat: 업체명 중복api 연동

--- src/api/admin/business/getCheckBusinessName.ts
+++ src/api/admin/business/getCheckBusinessName.ts
@@ -3,8 +3,8 @@ import api from '@/_lib/fetcher';
 import useAuthStore from '@/store/useAuthStore';
 
 export const getCheckBusinessName = async (
-	businessName: string,
 	authorization: string,
+	businessName: string,
 	setAccessToken: (accessToken: string) => void,
 ) => {
 	const response: IResponseType = await api.get({
