51899d95d0fe93a180645a6e412898e451ad7f28
naga361111 <naga361111@gmail.com>
2025-05-25 18:51:56 +0900

    resolve

--- src/_lib/apiRoutes.ts
+++ src/_lib/apiRoutes.ts
@@ -1,12 +1,10 @@
 export const apiRoutes = {
-	// 관리자 업체 조회
-	adminBusiness: '/admin/business',
-
 	// 인증관련
 	login: '/login',
 	reissue: '/token',
 
-	// 사용자
+	// 관리자 업체 조회
+	adminBusiness: '/admin/business',
 
 	// 공통
 	business: '/business'
