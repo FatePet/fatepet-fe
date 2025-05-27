6a2001d1a52f1b28cf59e52c5aa769bdea57651c
shuding0307 <suhyun2116@gmail.com>
2025-05-27 21:05:36 +0900

    feat: 업체수정 api 파일 작성

---
+++ src/api/admin/business/patchEditBusiness.ts
@@ -0,0 +1,71 @@
+import { apiRoutes } from '@/_lib/apiRoutes';
+import api from '@/_lib/fetcher';
+
+export const patchEditBusiness = async (
+	body: IPatchBusinessRequestType,
+	token: string,
+	setAccessToken: (accessToken: string) => void,
+) => {
+	const formData = new FormData();
+
+	if (body.name) formData.append('name', body.name);
+	if (body.category) formData.append('category', body.category);
+	if (body.mainImage) formData.append('mainImage', body.mainImage as File);
+	if (body.address) formData.append('address', body.address);
+	if (body.latitude) formData.append('latitude', String(body.latitude));
+	if (body.longitude) formData.append('longitude', String(body.longitude));
+	if (body.businessHours) formData.append('businessHours', body.businessHours);
+	if (body.phoneNumber) formData.append('phoneNumber', body.phoneNumber);
+	if (body.email) formData.append('email', body.email);
+
+	if (body.addService) {
+		formData.append('addService', JSON.stringify(body.addService));
+	}
+	if (body.addServiceImage && body.addServiceImage.length > 0) {
+		body.addServiceImage.forEach((file) => {
+			if (file) formData.append('addServiceImage', file);
+		});
+	}
+
+	if (body.updateService) {
+		formData.append('updateService', JSON.stringify(body.updateService));
+	}
+	if (body.updateServiceImage && body.updateServiceImage.length > 0) {
+		body.updateServiceImage.forEach((file) => {
+			if (file) formData.append('updateServiceImage', file);
+		});
+	}
+
+	if (body.removeServiceIds && body.removeServiceIds.length > 0) {
+		formData.append('removeServiceIds', JSON.stringify(body.removeServiceIds));
+	}
+
+	if (body.addAdditionalImage && body.addAdditionalImage.length > 0) {
+		body.addAdditionalImage.forEach((file) => {
+			if (file) formData.append('addAdditionalImage', file);
+		});
+	}
+
+	if (
+		body.removeAdditionalImageIds &&
+		body.removeAdditionalImageIds.length > 0
+	) {
+		formData.append(
+			'removeAdditionalImageIds',
+			JSON.stringify(body.removeAdditionalImageIds),
+		);
+	}
+
+	if (body.additionalInfo) {
+		formData.append('additionalInfo', body.additionalInfo);
+	}
+
+	const response = await api.post<FormData, IResponseType>({
+		endpoint: apiRoutes.admin,
+		authorization: token,
+		body: formData,
+		setAccessToken,
+	});
+
+	return response;
+};
---
+++ src/hooks/api/admin/business/usePatchEditBusiness.ts
@@ -0,0 +1,26 @@
+'use client';
+
+import { deleteAdminBusiness } from '@/api/admin/business/deleteAdminBusiness';
+import { patchEditBusiness } from '@/api/admin/business/patchEditBusiness';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+
+export const usePatchEditBusiness = (
+	authorization: string,
+	setAccessToken: (accessToken: string) => void,
+) => {
+	// const queryClient = useQueryClient();
+	const router = useRouter();
+
+	return useMutation({
+		mutationFn: (body: IPatchBusinessRequestType) =>
+			patchEditBusiness(body, authorization, setAccessToken),
+		onSuccess: (data: IResponseType) => {
+			alert(data.message);
+			router.back();
+		},
+		onError: (error) => {
+			alert(error.message);
+		},
+	});
+};
