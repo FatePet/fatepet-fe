1a21ef3a7ca2a656d68bc2f9eece21c72156f2d0
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-05-29 22:00:01 +0900

    fix: default를 인기순으로 바꿔 sortOption 함수 리액트 쿼리함수 내부로 이동시키고 default값을 POPULAR로 수정

--- src/hooks/api/user/useGetUserBusiness.ts
+++ src/hooks/api/user/useGetUserBusiness.ts
@@ -1,17 +1,28 @@
 'use client';
 
-import { getUserBusiness } from "@/api/user/business/getUserBusiness";
-import { useQuery } from "@tanstack/react-query";
+import { getUserBusiness } from '@/api/user/business/getUserBusiness';
+import { useQuery } from '@tanstack/react-query';
 
 export const useGetUserBusiness = (
-    sort: string,
-    page: number, 
-    size: number,
-    latitude: number | null, 
-    longitude: number | null,
+	sort: string,
+	page: number,
+	size: number,
+	latitude: number | null,
+	longitude: number | null,
 ) => {
-    return useQuery({
-        queryKey: ['USER_BUSINESS_LIST', sort],
-        queryFn: () => getUserBusiness(sort, page, size, latitude, longitude),
-    });
+	const sortOption: string = (() => {
+		switch (sort) {
+			case '거리순':
+				return 'DISTANCE';
+			case '추천순':
+				return 'RECOMMEND';
+			default:
+				return 'POPULAR';
+		}
+	})();
+
+	return useQuery({
+		queryKey: ['USER_BUSINESS_LIST', sortOption],
+		queryFn: () => getUserBusiness(sortOption, page, size, latitude, longitude),
+	});
 };
